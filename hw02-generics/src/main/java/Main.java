f03a4f1dfc38c9c009cfa371e476ef00c77b8afb
Maria Shirokova <m.shirokova@space307.com>
2019-10-27 16:57:24 +0300

    added more elements to list

--- hw02-generics/src/main/java/Main.java
+++ hw02-generics/src/main/java/Main.java
@@ -20,6 +20,30 @@ public class Main {
         Fruit grape = new Fruit("grape");
         Fruit kiwi = new Fruit("kiwi");
 
+        Fruit apple1 = new Fruit("apple");
+        Fruit orange1 = new Fruit("orange");
+        Fruit lemon1 = new Fruit("lemon");
+        Fruit banana1 = new Fruit("banana");
+        Fruit watermelon1 = new Fruit("watermelon");
+        Fruit mango1 = new Fruit("mango");
+        Fruit apricot1 = new Fruit("apricot");
+        Fruit nectarine1 = new Fruit("nectarine");
+        Fruit pear1 = new Fruit("pear");
+        Fruit grape1 = new Fruit("grape");
+        Fruit kiwi1 = new Fruit("kiwi");
+
+        Fruit apple2 = new Fruit("apple");
+        Fruit orange2 = new Fruit("orange");
+        Fruit lemon2 = new Fruit("lemon");
+        Fruit banana2 = new Fruit("banana");
+        Fruit watermelon2 = new Fruit("watermelon");
+        Fruit mango2 = new Fruit("mango");
+        Fruit apricot2 = new Fruit("apricot");
+        Fruit nectarine2 = new Fruit("nectarine");
+        Fruit pear2 = new Fruit("pear");
+        Fruit grape2 = new Fruit("grape");
+        Fruit kiwi2 = new Fruit("kiwi");
+
         Fruit strawberry = new Fruit("strawberry");
         Fruit cherry = new Fruit("cherry");
 
@@ -28,6 +52,36 @@ public class Main {
         fruits.add(lemon);
         fruits.add(banana);
         fruits.add(apricot);
+        fruits.add(watermelon);
+        fruits.add(mango);
+        fruits.add(nectarine);
+        fruits.add(pear);
+        fruits.add(grape);
+        fruits.add(kiwi);
+
+        fruits.add(apple1);
+        fruits.add(orange1);
+        fruits.add(lemon1);
+        fruits.add(banana1);
+        fruits.add(apricot1);
+        fruits.add(watermelon1);
+        fruits.add(mango1);
+        fruits.add(nectarine1);
+        fruits.add(pear1);
+        fruits.add(grape1);
+        fruits.add(kiwi1);
+
+        fruits.add(apple2);
+        fruits.add(orange2);
+        fruits.add(lemon2);
+        fruits.add(banana2);
+        fruits.add(apricot2);
+        fruits.add(watermelon2);
+        fruits.add(mango2);
+        fruits.add(nectarine2);
+        fruits.add(pear2);
+        fruits.add(grape2);
+        fruits.add(kiwi2);
 
         basket.addAll(fruits, strawberry, cherry);
 
